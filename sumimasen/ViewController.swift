c6235d7896fd05e80bcf37f5dc9442050a101a82
Hiroki Taniguchi <acmikn43@gmail.com>
2016-03-13 01:28:17 +0900

    Initial Commit

---
+++ sumimasen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sumimasen
+//
+//  Created by HIroki Taniguti on 2016/03/13.
+//  Copyright © 2016年 HIroki Taniguti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
