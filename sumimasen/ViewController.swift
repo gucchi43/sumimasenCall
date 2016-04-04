bf177160f03d6b21628bd65f76aa5d5a18604481
Hiroki Taniguchi <acmikn43@gmail.com>
2016-04-05 01:12:15 +0900

    firstcommit ボタン押したら音出る

--- sumimasen/ViewController.swift
+++ sumimasen/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 再生する audio ファイルのパスを取得
+        let audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("n109", ofType: "mp3")!)
+        
+        // auido を再生するプレイヤーを作成する
+        var audioError: NSError?
+        
+        //AVAudioPlayerのインスタンス化
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOfURL: audioPath, fileTypeHint: nil)
+        }catch{
+            print("Failed AVAudioPlayer Instance")
+        }
+        
+        
+        // エラーが起きたとき
+        if let error = audioError {
+            print("Error \(error.localizedDescription)")
+        }
+        
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+
     }
 
+    @IBAction func sumimasen(sender: AnyObject) {
+        print("ボタン押した")
+        if ( audioPlayer.playing ){
+            audioPlayer.stop()
+            button.setTitle("Stop", forState: UIControlState.Normal)
+            print("Stop押した")
+        }
+        else{
+            audioPlayer.play()
+            button.setTitle("Play", forState: UIControlState.Normal)
+            print("Play押した")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
