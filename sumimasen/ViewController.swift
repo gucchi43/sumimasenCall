9a631f38250097f6afde6bcb875475835162ae0a
Hiroki Taniguchi <acmikn43@gmail.com>
2016-08-17 13:57:32 +0900

    tinderみたいな波紋アニメーションを実装&pod組み込み  - pod (pulastor, colors)

--- sumimasen/ViewController.swift
+++ sumimasen/ViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 import AVFoundation
+import Pulsator
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer: AVAudioPlayer!
-    
+    let pulsator = Pulsator()
+
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var pulstorView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +33,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         }catch{
             print("Failed AVAudioPlayer Instance")
         }
-        
-        
+
         // エラーが起きたとき
         if let error = audioError {
             print("Error \(error.localizedDescription)")
@@ -40,21 +42,42 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         audioPlayer.delegate = self
         audioPlayer.prepareToPlay()
 
+        pulstorView.layer.superlayer?.insertSublayer(pulsator, below: pulstorView.layer)
+        setupInitialValues()
+        let width = button.frame.width
+        button.layer.cornerRadius = width / 2 // サイズの半分の値
+
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        pulstorView.layer.layoutIfNeeded()
+        pulsator.position = pulstorView.layer.position
+    }
+
+    private func setupInitialValues() {
+        pulsator.numPulse = 3
+        pulsator.radius = 500.0
+        let pulsatorColor = UIColor.purpleColor().CGColor
+        pulsator.backgroundColor = pulsatorColor
     }
 
     @IBAction func sumimasen(sender: AnyObject) {
         print("ボタン押した")
         if (audioPlayer.playing ){
-//            audioPlayer.stop()
             audioPlayer.currentTime = NSTimeInterval(0)
-//            button.setTitle("おわりじゃないもん", forState: UIControlState.Normal)
             print("Stop押した")
         }
         else{
             audioPlayer.play()
-//            button.setTitle("おわりじゃないもん", forState: UIControlState.Normal)
             print("Play押した")
+            startPlustor()
         }
+    }
+
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        stopPlustor()
     }
     
     override func didReceiveMemoryWarning() {
@@ -62,6 +85,14 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    func startPlustor() {
+        print("startPlustor呼び出し")
+        pulsator.start()
+    }
 
+    func stopPlustor() {
+        print("stopPlustor呼び出し")
+        pulsator.stop()
+    }
 }
 
